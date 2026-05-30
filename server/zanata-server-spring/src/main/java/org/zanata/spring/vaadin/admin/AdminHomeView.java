8683847dab4328753a7f61f5390e1c0e3644f76b
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-05-30 16:30:48 +0300

    reorder view

--- server/zanata-server-spring/src/main/java/org/zanata/spring/vaadin/admin/AdminHomeView.java
+++ server/zanata-server-spring/src/main/java/org/zanata/spring/vaadin/admin/AdminHomeView.java
@@ -1,6 +1,9 @@
 package org.zanata.spring.vaadin.admin;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.zanata.spring.i18n.TitleKey;
@@ -8,6 +11,7 @@ import com.vaadin.flow.router.RouterLink;
 import jakarta.annotation.security.RolesAllowed;
 
 import org.zanata.spring.vaadin.MainLayout;
+import org.zanata.spring.vaadin.theme.AuraUtility;
 
 @Route(value = "admin/home", layout = MainLayout.class)
 @RolesAllowed("ADMIN")
@@ -15,27 +19,65 @@ public class AdminHomeView extends VerticalLayout implements TitleKey {
 
     @Override public String pageTitleKey() { return "page.administration"; }
 
-
     public AdminHomeView() {
         setSizeFull();
         setPadding(true);
+        setSpacing(true);
 
         add(new H2(getTranslation("page.administration")));
-        add(new RouterLink(getTranslation("page.homeContent"),     AdminHomeContentView.class));
-        add(new RouterLink(getTranslation("page.myProjects"),      AdminProjectsView.class));
-        add(new RouterLink(getTranslation("page.groups"),          AdminGroupsView.class));
-        add(new RouterLink(getTranslation("page.serverSettings"),  AdminServerSettingsView.class));
-        add(new RouterLink(getTranslation("page.aiTranslation"),   AdminAiSettingsView.class));
-        add(new RouterLink(getTranslation("page.userManager"),     AdminUserManagerView.class));
-        add(new RouterLink(getTranslation("page.createUser"),      AdminCreateUserView.class));
-        add(new RouterLink(getTranslation("page.roleManager"),     AdminRoleManagerView.class));
-        add(new RouterLink(getTranslation("page.roleRules"),       AdminRoleRulesView.class));
-        add(new RouterLink(getTranslation("page.requests"),        AdminRequestsView.class));
-        add(new RouterLink(getTranslation("page.reviewCriteria"),  AdminReviewView.class));
-        add(new RouterLink(getTranslation("admin.menu.searchReindex"), AdminSearchView.class));
-        add(new RouterLink(getTranslation("page.cacheStats"),      AdminCacheStatsView.class));
-        add(new RouterLink(getTranslation("admin.menu.stats"),     AdminStatsView.class));
-        add(new RouterLink(getTranslation("page.processManager"),  AdminProcessManagerView.class));
-        add(new RouterLink(getTranslation("page.monitoring"),      AdminMonitoringView.class));
+
+        // Vertical stack of titled blocks (the view itself is a VerticalLayout).
+        add(block("admin.section.users",
+                link("page.userManager", AdminUserManagerView.class),
+                link("page.createUser", AdminCreateUserView.class),
+                link("page.roleManager", AdminRoleManagerView.class),
+                link("page.roleRules", AdminRoleRulesView.class),
+                link("page.requests", AdminRequestsView.class)));
+
+        add(block("admin.section.projects",
+                link("page.myProjects", AdminProjectsView.class),
+                link("page.groups", AdminGroupsView.class),
+                link("page.homeContent", AdminHomeContentView.class),
+                link("page.reviewCriteria", AdminReviewView.class)));
+
+        add(block("admin.section.server",
+                link("page.serverSettings", AdminServerSettingsView.class),
+                link("page.aiTranslation", AdminAiSettingsView.class),
+                link("admin.menu.searchReindex", AdminSearchView.class),
+                link("page.cacheStats", AdminCacheStatsView.class),
+                link("admin.menu.stats", AdminStatsView.class),
+                link("page.processManager", AdminProcessManagerView.class),
+                link("page.monitoring", AdminMonitoringView.class)));
+    }
+
+    /** A titled card containing a column of navigation links. */
+    private Div block(String titleKey, RouterLink... links) {
+        Div card = new Div();
+        card.addClassNames(AuraUtility.Border.ALL, AuraUtility.BorderColor.DEFAULT,
+                AuraUtility.BorderRadius.MEDIUM, AuraUtility.Padding.MEDIUM,
+                AuraUtility.Background.BASE, AuraUtility.BoxSizing.BORDER);
+        card.getStyle().set("width", "100%");
+        card.getStyle().set("max-width", "640px");
+
+        H3 title = new H3(getTranslation(titleKey));
+        title.addClassNames(AuraUtility.Margin.NONE,
+                AuraUtility.Margin.Bottom.MEDIUM);
+
+        VerticalLayout body = new VerticalLayout(title);
+        body.setPadding(false);
+        body.setSpacing(false);
+        for (RouterLink l : links) {
+            body.add(l);
+        }
+        card.add(body);
+        return card;
+    }
+
+    private RouterLink link(String key, Class<? extends Component> target) {
+        @SuppressWarnings({ "unchecked", "rawtypes" })
+        RouterLink link = new RouterLink(getTranslation(key), (Class) target);
+        link.addClassNames(AuraUtility.Padding.Vertical.SMALL,
+                AuraUtility.TextDecoration.NONE);
+        return link;
     }
 }
